14f2b6787b524c5dc5ce483aec274e8bee42e2f9
Daniel Valderrama <danimanri05@gmail.com>
2024-11-07 20:56:16 -0600

    cors

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,10 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
