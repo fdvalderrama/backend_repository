a03ab1cf67ee046a9a391bc7e683757ac35bfb7e
Daniel Valderrama <danimanri05@gmail.com>
2024-11-07 20:17:54 -0600

    getAll

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,23 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get("/getAll", (req, res) => {
+  const filePath = path.join(__dirname, "productos.json");
+
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      console.error("Error reading productos.json:", err);
+      res.status(500).send("Error reading file");
+    } else {
+      const products = JSON.parse(data);
+      res.json(products);
+    }
+  });
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
