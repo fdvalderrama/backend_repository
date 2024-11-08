63e0aeaf6ce49343f5449af90d0775d12a6f9dbb
Daniel Valderrama <danimanri05@gmail.com>
2024-11-07 21:13:14 -0600

    segunda api

--- api/index.ts
+++ api/index.ts
@@ -45,6 +45,20 @@ app.get("/api/items", (req, res) => {
   });
 });
 
+const productsPath = path.join(__dirname, "products.json");
+const products = JSON.parse(fs.readFileSync(productsPath, "utf-8"));
+
+app.get("/api/items/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = products.find((p) => p.id === productId);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ error: "Product not found" });
+  }
+});
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
